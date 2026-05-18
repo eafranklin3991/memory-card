1d8cc83cfba81c940f37cf11cb827d725a86f1f2
eafranklin3991 <eafranklin3991@gmail.com>
2026-05-18 17:10:47 -0400

    Lay cards out in two rows

--- src/components/CardTable.jsx
+++ src/components/CardTable.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import Card from './Card';
+import '../styles/CardTable.css'
 
 function CardTable() {
     return (
         <div className='cardTable'>
-            <Card />
+            {Array(10).fill(true).map((_, i) => <Card key={i} />)}
         </div>
     )
 }
