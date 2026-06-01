bd8f4069ad28d5903832222d554c74ef172ce0f2
eafranklin3991 <eafranklin3991@gmail.com>
2026-06-01 15:19:47 -0400

    Fixed subtle bug related to mutating doubleArray during shuffle function

--- src/components/CardTable.jsx
+++ src/components/CardTable.jsx
@@ -3,29 +3,25 @@ import Card from './Card';
 import '../styles/CardTable.css'
 
 function CardTable() {
-    const pokemonArray = Array(5).fill(true).map(() => Math.ceil(Math.random() * 151));
+    const pokemonArray = Array(5).fill(null).map(() => Math.ceil(Math.random() * 151));
     const doubleArray = [...pokemonArray, ...pokemonArray];
 
     function shuffle(array) {
+        const shuffled = [...array];
         let currentIndex = array.length;
-
-        // While there remain elements to shuffle...
         while (currentIndex != 0) {
-
-            // Pick a remaining element...
             let randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex--;
-
-            // And swap it with the current element.
-            [array[currentIndex], array[randomIndex]] = [
-                array[randomIndex], array[currentIndex]];
+            [shuffled[currentIndex], shuffled[randomIndex]] = [
+                shuffled[randomIndex], shuffled[currentIndex]];
         }
+        return shuffled;
     }
 
     const shuffledArray = shuffle(doubleArray);
 
     const cardArray = Array(10).fill(true).map((_, i) => {
-        const pokemonId = doubleArray[i];
+        const pokemonId = shuffledArray[i];
         return (
             <Card pokemonId={pokemonId} key={i} />
         )
