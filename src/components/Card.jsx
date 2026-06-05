b0e619a3f675b8ddd0784f34872ba9ff54603b36
eafranklin3991 <eafranklin3991@gmail.com>
2026-06-05 17:27:48 -0400

    handleCardFlip not working - using if else

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import CardProfile from './CardProfile';
 import '../styles/Card.css';
 
-function Card({ pokemonId, isRevealed, isMatched, onClick }) {
+function Card({ pokemonId, isRevealed, onClick }) {
     return (
-        <div className='card' onClick={onClick} data-revealed={isRevealed} data-matched={isMatched}>
-            <div className="profile-container">
-                {isRevealed && <CardProfile pokemonId={pokemonId} />}
+        <div className='card'>
+            <div className='profile-container'>
+                {isRevealed
+                    ? <CardProfile pokemonId={pokemonId} />
+                    : <div className='card-back'></div>}
             </div>
         </div>
     )
--- src/components/CardTable.jsx
+++ src/components/CardTable.jsx
@@ -1,47 +1,65 @@
-import { useState, Fragment } from 'react'
+import { useState, useRef } from 'react'
 import Card from './Card';
 import '../styles/CardTable.css'
 
-function CardTable() {
-    const [revealedIndex, setRevealedIndex] = useState(null);
-    const [matchedIndex, setMatchedIndex] = useState(null);
-
-    const pokemonArray = Array(5).fill(null).map(() => Math.ceil(Math.random() * 151));
-    const doubleArray = [...pokemonArray, ...pokemonArray];
-
-    function shuffle(array) {
-        const shuffled = [...array];
-        let currentIndex = array.length;
-        while (currentIndex != 0) {
-            let randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex--;
-            [shuffled[currentIndex], shuffled[randomIndex]] = [
-                shuffled[randomIndex], shuffled[currentIndex]];
-        }
-        return shuffled;
+function shuffle(array) {
+    const shuffled = [...array];
+    let currentIndex = array.length;
+    while (currentIndex != 0) {
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [shuffled[currentIndex], shuffled[randomIndex]] = [
+            shuffled[randomIndex], shuffled[currentIndex]];
     }
+    return shuffled;
+}
 
-    const shuffledArray = shuffle(doubleArray);
+function initCards(numMatches) {
+    const ids = Array(numMatches).fill(null).map(() => Math.ceil(Math.random() * 151));
+    return shuffle([...ids, ...ids]).map((pokemonId, i) => ({ id: i, pokemonId }));
+}
 
-    const handleCardFlip = () => {
-        revealedIndex.push(pairId);
-        if (new Set(revealedIndex).size !== revealedIndex.length):
-            matchedIndex.push(pairId);
+function CardTable() {
+    const cards = useRef(initCards(5));
+    const [revealed, setRevealed] = useState([{ id: "", pokemonId: "" }]);
+    const [matched, setMatched] = useState([{ id: "", pokemonId: "" }]);
 
-        setRevealedIndex(pairId);
-    };
+    const handleCardflip = (index) => {
+        const card = cards.current[index];
 
-    const cardArray = Array(10).fill(true).map((_, i) => {
-        const pokemonId = shuffledArray[i];
-        const pairId = doubleArray[i];
-        return (
-            <Card pokemonId={pokemonId} key={i} pairId={pairId} isMatched={matchedIndex === pairId} onClick={handleCardFlip} isRevealed={revealedIndex === pairId} />
-        )
-    })
+        // If there are no cards flipped, stay revealed
+        if (revealed.length === 0) {
+            const newRevealed = [{ id: card.id, pokemonId: card.pokemonId }]
+            setRevealed(newRevealed);
+        }
+
+        // If there is only one card flipped and the same card is clicked again, flip back
+        if (revealed["id"].includes(card.id)) {
+            const newRevealed = revealed["id"].filter(item => item !== card.id)
+            setRevealed(newRevealed);
+        }
+        // If there are two cards flipped and a match, stay revealed
+        else if (revealed["pokemonId"].includes(card.pokemonId)) {
+            const newRevealed = [...revealed, { id: card.id, pokemonId: card.pokemonId }]
+            setRevealed(newRevealed);
+        }
+        // If there are two cards flipped and no matches, flip back
+        else {
+            return
+        };
+    }
 
     return (
         <div className='cardTable'>
-            {cardArray}
+            {cards.current.map((card, index) =>
+                <Card
+                    key={card.id}
+                    pokemonId={card.pokemonId}
+                    isRevealed={revealed.includes(index)}
+                    onClick={() => handleCardflip(index)}
+                ></Card>
+            )
+            }
         </div>
     )
 }
