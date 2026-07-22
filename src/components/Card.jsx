45f5dc9946df3afabdf499dafce4fcd624febe09
Emily Franklin <eafranklin3991@gmail.com>
2026-07-22 17:26:14 -0400

    Clean up

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,9 +4,9 @@ import '../styles/Card.css';
 function Card({ pokemonId, isRevealed, isMatched, onClick }) {
     return (
         <div className='card' data-revealed={isRevealed} data-matched={isMatched} onClick={onClick}>
-                {isRevealed | isMatched
-                    ? <CardProfile pokemonId={pokemonId} />
-                    : <div className='card-back'></div>}
+            {isRevealed | isMatched
+                ? <CardProfile pokemonId={pokemonId} />
+                : <div className='card-back'></div>}
         </div>
     )
 }
