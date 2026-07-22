94c992d2c71f4c56e79b6a8665cba75608e42720
Emily Franklin <eafranklin3991@gmail.com>
2026-07-22 16:29:49 -0400

    No more profile-container div, not necessary

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
 import CardProfile from './CardProfile';
 import '../styles/Card.css';
 
 function Card({ pokemonId, isRevealed, isMatched, onClick }) {
     return (
         <div className='card' data-revealed={isRevealed} data-matched={isMatched} onClick={onClick}>
-            <div className='profile-container'>
                 {isRevealed | isMatched
                     ? <CardProfile pokemonId={pokemonId} />
                     : <div className='card-back'></div>}
-            </div>
         </div>
     )
 }
