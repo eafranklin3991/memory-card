b3766967170774fd0b179e6f6b9139124e0f0ee3
Emily Franklin <eafranklin3991@gmail.com>
2026-07-24 16:21:18 -0400

    Restart button works

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
-import '../styles/App.css'
-import CardTable from './CardTable';
-import { useState } from 'react';
+import "../styles/App.css";
+import CardTable from "./CardTable";
+import { useState } from "react";
 
 function App() {
   const [gameWon, setGameWon] = useState(false);
@@ -10,42 +10,58 @@ function App() {
     const shuffled = [...array];
     let currentIndex = array.length;
     while (currentIndex != 0) {
-        let randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-        [shuffled[currentIndex], shuffled[randomIndex]] = [
-            shuffled[randomIndex], shuffled[currentIndex]];
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [shuffled[currentIndex], shuffled[randomIndex]] = [
+        shuffled[randomIndex],
+        shuffled[currentIndex],
+      ];
     }
     return shuffled;
-}
+  }
 
-function initCards(numMatches) {
+  function initCards(numMatches) {
     let idSet = new Set();
 
     // Ensure there aren't duplicate pokemon ids
     while (idSet.size < numMatches) {
-        const potential_id = Math.ceil(Math.random() * 151);
-        idSet.add(potential_id);
+      const potential_id = Math.ceil(Math.random() * 151);
+      idSet.add(potential_id);
     }
 
     const idArray = Array.from(idSet);
-    
-    return shuffle([...idArray, ...idArray]).map((pokemonId, i) => ({ id: i, pokemonId }));
-}
+
+    return shuffle([...idArray, ...idArray]).map((pokemonId, i) => ({
+      id: i,
+      pokemonId,
+    }));
+  }
   const [numMatches] = useState(5);
-  const [cards] = useState(() => initCards(numMatches));
+  const [cards, setCards] = useState(() => initCards(numMatches));
   function restartGame() {
+    setCards(() => initCards(numMatches));
     setRevealed([]);
     setMatched([]);
     setGameWon(false);
   }
   return (
-    <div className='wrapper'>
-      <h1 className='game-title'>Memory Card Matching Game</h1>
-      <button className='restart-btn' onClick={restartGame}>Restart</button>
-      {gameWon && <h3 className='game-message'>All Cards Matched!</h3>}
-      <CardTable cards={cards} numMatches={numMatches} setGameWon={setGameWon} revealed={revealed} setRevealed={setRevealed} matched={matched} setMatched={setMatched}/>
+    <div className="wrapper">
+      <h1 className="game-title">Memory Card Matching Game</h1>
+      <button className="restart-btn" onClick={restartGame}>
+        Restart
+      </button>
+      {gameWon && <h3 className="game-message">All Cards Matched!</h3>}
+      <CardTable
+        cards={cards}
+        numMatches={numMatches}
+        setGameWon={setGameWon}
+        revealed={revealed}
+        setRevealed={setRevealed}
+        matched={matched}
+        setMatched={setMatched}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
